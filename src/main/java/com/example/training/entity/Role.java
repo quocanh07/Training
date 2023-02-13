1d52623557d4da7bbfaebc71a07772786c0ca9a8
quocanh07 <anhquoc71202@gmail.com>
2023-02-13 13:13:12 +0700

    model

--- src/main/java/com/example/training/entity/Role.java
+++ src/main/java/com/example/training/entity/Role.java
@@ -1,2 +1,42 @@
-package com.example.training.entity;public class Role {
+package com.example.training.entity;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@Data
+@NoArgsConstructor
+@Table(name = "Roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    @ManyToMany(mappedBy = "roles")
+    @Fetch(value= FetchMode.SELECT)
+    @JsonIgnore
+    private Set<User> employees = new HashSet<>();
+
+    public Role(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
 }
